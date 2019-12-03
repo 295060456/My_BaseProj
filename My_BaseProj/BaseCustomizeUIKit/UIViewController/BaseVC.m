7e09fbd4175e700404d0313f04cf11ca5ad472bd
Kite <>
2019-12-03 12:19:59 +0800

    no message

--- My_BaseProj/BaseCustomizeUIKit/UIViewController/BaseVC.m
+++ My_BaseProj/BaseCustomizeUIKit/UIViewController/BaseVC.m
@@ -103,6 +103,7 @@ JXCategoryListContentViewDelegate
 -(void)viewDidDisappear:(BOOL)animated{
     [super viewDidDisappear:animated];
     [SVProgressHUD dismiss];
+    printf("retain count = %ld\n",CFGetRetainCount((__bridge CFTypeRef)(self)));//打印对象的引用计数器
 }
 
 -(UIStatusBarStyle)preferredStatusBarStyle{

8da3b992b0fbb8db7573f67b9f257aadebed95c1
Jobs <>
2020-07-07 16:11:20 +0700

    no message

--- My_BaseProj/BaseCustomizeUIKit/UIButton/UIButton+Block.h
+++ My_BaseProj/BaseCustomizeUIKit/UIButton/UIButton+Block.h
@@ -6,27 +6,20 @@ typedef void(^ButtonBlock)(UIButton* btn);
 
 /**
  *  button 添加点击事件
- *
- *  @param block
  */
 - (void)addAction:(ButtonBlock)block;
 
-
 /// 添加背景圖可以旋轉 Pi/2 的Button
-/// @param block
 - (void)addActionAutoImage:(ButtonBlock)block;
 
 /**
  *  button 添加事件
- *
- *  @param block
  *  @param controlEvents 点击的方式
  */
-- (void)addAction:(ButtonBlock)block forControlEvents:(UIControlEvents)controlEvents;
-
+- (void)addAction:(ButtonBlock)block
+ forControlEvents:(UIControlEvents)controlEvents;
 
 /// 添加背景圖可以旋轉 Pi 的Button
-/// @param block
 - (void)addActionAutoImageWithPI:(ButtonBlock)block;
 
 @end

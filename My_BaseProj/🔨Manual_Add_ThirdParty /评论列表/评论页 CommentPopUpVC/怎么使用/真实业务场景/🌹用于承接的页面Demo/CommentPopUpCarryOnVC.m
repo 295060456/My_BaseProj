b99d2d61b33d048f8e7af3b4c7634afdd32ef3de
Jobs <>
2020-07-29 19:22:09 +0700

    no message

--- My_BaseProj/🔨Manual_Add_ThirdParty /评论列表/评论页 CommentPopUpVC/怎么使用/真实业务场景/🌹用于承接的页面Demo/CommentPopUpCarryOnVC.m
+++ My_BaseProj/🔨Manual_Add_ThirdParty /评论列表/评论页 CommentPopUpVC/怎么使用/真实业务场景/🌹用于承接的页面Demo/CommentPopUpCarryOnVC.m
@@ -247,8 +247,8 @@
         [self.view addSubview:_commentPopUpVC.view];
         
         [_commentPopUpVC commentPopUpActionBlock:^(id data) {
-            NSLog(@"");
-            [self willClose_vertical];
+            //键盘消失
+            [self.commentPopUpVC.inputView endEditing:YES];
         }];
         
         [_commentPopUpVC popUpActionBlock:^(id data) {

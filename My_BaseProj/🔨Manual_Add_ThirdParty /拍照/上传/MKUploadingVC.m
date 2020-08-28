5d07b3646d786e288c3a3db3b874835c0d7f3eb7
Jobs <>
2020-08-28 16:19:52 +0700

    no message

--- My_BaseProj/🔨Manual_Add_ThirdParty /拍照/上传/MKUploadingVC.m
+++ My_BaseProj/🔨Manual_Add_ThirdParty /拍照/上传/MKUploadingVC.m
@@ -272,7 +272,7 @@ shouldChangeTextInRange:(NSRange)range
         _textView.backgroundColor = COLOR_RGB(33, 38, 50, 1);
         _textView.delegate = self;
         _textView.attributedPlaceholder = [[NSMutableAttributedString alloc] initWithString:@"主人来两句嘛！~~~"
-                                                                                 attributes:@{NSFontAttributeName:[UIFont fontWithName:@"PingFangSC" size: 15],
+                                                                                 attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:15 weight:UIFontWeightRegular],
                                                                                               NSForegroundColorAttributeName:kWhiteColor}];
         _textView.placeholderTextColor = kWhiteColor;
         _textView.textColor = kWhiteColor;
@@ -290,7 +290,7 @@ shouldChangeTextInRange:(NSRange)range
         _tipsLab = UILabel.new;
         _tipsLab.textColor = kWhiteColor;//
         _tipsLab.attributedText = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"还可以输入%d个字符",self.inputLimit]
-                                                                         attributes:@{NSFontAttributeName:[UIFont fontWithName:@"PingFangSC" size: 15],
+                                                                         attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:15 weight:UIFontWeightRegular],
                                                                                       NSForegroundColorAttributeName:HEXCOLOR(0x242A37)}];
         [self.backView addSubview:_tipsLab];
         [_tipsLab mas_makeConstraints:^(MASConstraintMaker *make) {

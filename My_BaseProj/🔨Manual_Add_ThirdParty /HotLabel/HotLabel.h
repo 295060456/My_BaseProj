acced60c50e4ce885361a2464eb74ba11a08e218
Jobs <>
2020-10-05 21:16:52 +0700

    no message

--- My_BaseProj/🔨Manual_Add_ThirdParty /HotLabel/HotLabel.h
+++ My_BaseProj/🔨Manual_Add_ThirdParty /HotLabel/HotLabel.h
@@ -1,9 +1,8 @@
 //
 //  HotLabel.h
-//  My_BaseProj
+//  JobsSearch
 //
-//  Created by Administrator on 08/06/2019.
-//  Copyright © 2019 Administrator. All rights reserved.
+//  Created by Jobs on 2020/10/4.
 //
 
 #import <UIKit/UIKit.h>
@@ -12,16 +11,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface HotLabel : UIView
 
--(instancetype)initWithTopOffset:(CGFloat)topOffset
-                      eachOffset:(CGFloat)eachOffset;
+@property(nonatomic,assign)CGFloat top;//第一个元素距离HotLabel上边的距离  同样也是下边距（对称性）
+@property(nonatomic,assign)CGFloat left;//第一个元素距离HotLabel左边的距离 同样也是右边距（对称性）
+@property(nonatomic,assign)CGFloat offsetXForEach;//控件之间的左右距离
+@property(nonatomic,assign)CGFloat offsetYForEach;//控件之间的上下距离
+@property(nonatomic,strong)UIFont *btnTitleFont;//按钮的字体
+@property(nonatomic,strong)UIColor *btnTitleColor;//按钮的字体颜色
+@property(nonatomic,strong)NSArray *titleArr;
 
--(void)hotLabelWithDataArr:(NSArray *)dataArr;
-
--(void)actionBlock:(MKDataBlock)Block;
+-(void)actionBlockHotLabel:(MKDataBlock)hotLabelBlock;//点击事件传递
+-(CGFloat)heightForHotLabel;//约束走完以后，计算得出的高，用于外界自适应
 
 @end
 
 NS_ASSUME_NONNULL_END
-
-//1、根据内容对承载物Lable的宽进行动态设置;
-//2、一行放不下自动转入下一行
--- My_BaseProj/🔨Manual_Add_ThirdParty /HotLabel/HotLabel.m
+++ My_BaseProj/🔨Manual_Add_ThirdParty /HotLabel/HotLabel.m
@@ -1,111 +1,166 @@
 //
 //  HotLabel.m
-//  My_BaseProj
+//  JobsSearch
 //
-//  Created by Administrator on 08/06/2019.
-//  Copyright © 2019 Administrator. All rights reserved.
+//  Created by Jobs on 2020/10/4.
 //
 
 #import "HotLabel.h"
+#import "UILabel+Extra.h"
 
 @interface HotLabel (){
-    
+    CGSize btnSize;
 }
 
-//内容物对父控件而言
-@property(nonatomic,assign)CGFloat topOffset;//上边距
-//内容物之间
-@property(nonatomic,assign)CGFloat eachOffset;//标签互相之间的距离
-@property(nonatomic,assign)NSInteger row;
-@property(nonatomic,strong)NSMutableArray *labelMutArr;
-//动态运算值————当前状态下最后一个label,右下角的x轴坐标
-@property(nonatomic,assign)CGFloat lastPoint;
+@property(nonatomic,copy)MKDataBlock hotLabelBlock;
 
-@property(nonatomic,copy)MKDataBlock heightBlock;
+@property(nonatomic,strong)UIScrollView *scrollView;//所有控件加在这上面
+@property(nonatomic,strong)NSMutableArray <UIButton *>*btnMutArr;
+@property(nonatomic,assign)BOOL isOK;
+@property(nonatomic,assign)CGFloat X;//如果加载了下一个btn，那么直到他的尾巴处的x值，记住包含两边固有的间距进行比较
+@property(nonatomic,assign)int row;
+@property(nonatomic,assign)CGFloat hotLabelHeight;
 
 @end
 
 @implementation HotLabel
 
--(instancetype)initWithTopOffset:(CGFloat)topOffset
-                       eachOffset:(CGFloat)eachOffset{
-    
+-(instancetype)init{
     if (self = [super init]) {
-        
-        self.topOffset = topOffset;
-        
-        self.eachOffset = eachOffset;
-        
-        self.lastPoint = 0.00f;
-        
-        self.row = 0;
-        
+        self.backgroundColor = HEXCOLOR(0xFFFFFF);
     }return self;
 }
 
--(void)actionBlock:(MKDataBlock)Block{
-    
-    self.heightBlock = Block;
+-(void)drawRect:(CGRect)rect{
+    [super drawRect:rect];
+    if (!self.isOK) {
+        self.scrollView.alpha = 1;
+        [self createHotLabelWithArr:self.titleArr];
+        self.isOK = YES;
+    }
 }
 
--(void)hotLabelWithDataArr:(NSArray *)dataArr{
-    
-    //label.mj_w + 间隔(eachOffset) = 一组
-    for (int i = 0; i < dataArr.count; i++) {
-
-        UILabel *lable = UILabel.new;
-        
-        lable.backgroundColor = RandomColor;
-
-        lable.text = dataArr[i];
-        
-        [lable sizeToFit];//根据数据撑开Lable 得到其Size值
-        
-        [self addSubview:lable];
-        
-        UILabel *lab = self.labelMutArr.lastObject;
-        
-        if (lab) {
+-(void)createHotLabelWithArr:(NSArray <NSString *>*)dataArr{
+    if (dataArr.count) {
+        for (NSString *titleStr in dataArr) {
+            UIButton *btn = UIButton.new;
+            [btn setTitle:titleStr
+                 forState:UIControlStateNormal];
+            [btn setBackgroundImage:[UIImage imageWithColor:RandomColor] forState:UIControlStateNormal];//直接赋值是不行的
+            btn.titleLabel.font = self.btnTitleFont;
+            btn.titleLabel.textColor = self.btnTitleColor;
+            [btn.titleLabel sizeToFit];
+            btn.titleLabel.adjustsFontSizeToFitWidth = YES;
+            [UIView cornerCutToCircleWithView:btn AndCornerRadius:3];
             
-            self.lastPoint = lab.mj_x + lab.mj_w;
+            btnSize = [UILabel sizeWithText:titleStr
+                                       font:self.btnTitleFont
+                                    maxSize:CGSizeMake(MAXFLOAT, MAXFLOAT)];
             
-            if (self.lastPoint + lable.mj_w > self.mj_x + self.mj_w) {//提行
-                
-                ++self.row;
-                
-                lable.origin = CGPointMake(self.mj_x, self.topOffset + self.row * (lable.mj_h + self.eachOffset));
-                
-            }else{//本行执行
-                
-                UILabel *lab = self.labelMutArr.lastObject;
-                
-                lable.origin = CGPointMake(lab.mj_x + lab.mj_w + self.eachOffset, lab.mj_y);
+            @weakify(self)
+            [[btn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIButton * _Nullable x) {
+                @strongify(self)
+                NSLog(@"%@",x.titleLabel.text);
+                if (self.hotLabelBlock) {
+                    self.hotLabelBlock(x);//利用titleStr来进行判别
+                }
+            }];
+            [self.scrollView addSubview:btn];
+            if (self.btnMutArr.count) {
+                self.X += btnSize.width + self.offsetXForEach;
+                NSLog(@"self.X = %f",self.X);
+                [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+                    UIButton *lastBtn = (UIButton *)self.btnMutArr.lastObject;
+                    if (self.X <= self.mj_w - self.left) {//在本行排列
+                        make.top.equalTo(lastBtn);
+                        make.left.equalTo(lastBtn.mas_right).offset(self.offsetXForEach);
+                    }else{//换行从头排列
+                        
+                        self.row += 1;
+                        self.X = self.left + btnSize.width;//换行了 self.X 重置
+                        
+                        make.top.equalTo(lastBtn.mas_bottom).offset(self.offsetYForEach);
+                        make.left.equalTo(self).offset(self.left);
+                    }
+                }];
+            }else{//第一次
+                self.X = self.left + (btnSize.width + self.offsetXForEach);
+                self.row = 1;
+                [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+                    make.top.equalTo(self).offset(self.top);
+                    make.left.equalTo(self).offset(self.left);
+                }];
             }
-        }else{
-            
-            lable.origin = CGPointMake(self.mj_x, self.topOffset);//第一个基准值
+            [self.btnMutArr addObject:btn];
         }
         
-        [self.labelMutArr addObject:lable];
+        self.hotLabelHeight = self.top * 2 + btnSize.height * self.row + (self.row - 1) * self.offsetYForEach;
+        NSLog(@"self.row = %d",self.row);
         
-//        NSLog(@"%f",self.lastPoint);
-    }
-    
-    if (self.heightBlock) {
-        
-        self.heightBlock(self.labelMutArr.lastObject);
+        [[NSNotificationCenter defaultCenter] postNotificationName:ReuseIdentifier
+                                                            object:nil
+                                                          userInfo:@{@"hotLabelHeight":@(self.hotLabelHeight)}];
     }
 }
 
--(NSMutableArray *)labelMutArr{
-    
-    if (!_labelMutArr) {
-        
-        _labelMutArr = NSMutableArray.array;
-    }
-    
-    return _labelMutArr;
+-(CGFloat)heightForHotLabel{
+    return self.hotLabelHeight;
 }
 
+-(void)actionBlockHotLabel:(MKDataBlock)hotLabelBlock{
+    _hotLabelBlock = hotLabelBlock;
+}
+#pragma mark —— lazyLoad
+-(NSMutableArray<UIButton *> *)btnMutArr{
+    if (!_btnMutArr) {
+        _btnMutArr = NSMutableArray.array;
+    }return _btnMutArr;
+}
+
+-(UIScrollView *)scrollView{
+    if (!_scrollView) {
+        _scrollView = UIScrollView.new;
+        [self addSubview:_scrollView];
+        [_scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self);
+        }];
+    }return _scrollView;
+}
+
+-(CGFloat)top{
+    if (_top == 0) {
+        _top = 5;
+    }return _top;
+}
+
+-(CGFloat)left{
+    if (_left == 0) {
+        _left = 5;
+    }return _left;
+}
+
+-(CGFloat)offsetXForEach{
+    if (_offsetXForEach == 0) {
+        _offsetXForEach = 8;
+    }return _offsetXForEach;
+}
+
+-(CGFloat)offsetYForEach{
+    if (_offsetYForEach == 0) {
+        _offsetYForEach = 8;
+    }return _offsetYForEach;
+}
+
+-(UIFont *)btnTitleFont{
+    if (!_btnTitleFont) {
+        _btnTitleFont = kFontSize(20);
+    }return _btnTitleFont;
+}
+
+-(UIColor *)btnTitleColor{
+    if (!_btnTitleColor) {
+        _btnTitleColor = kWhiteColor;
+    }return _btnTitleColor;
+}
 
 @end

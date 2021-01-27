1ab0af182883c11a84a33165883d5ba74d9afab8
Jobs <lg295060456@gmail.com>
2021-01-27 12:09:19 +0700

    no message

--- My_BaseProj/🌹Macro/第三方头文件管理/Pods/Pods.h
+++ My_BaseProj/🌹Macro/第三方头文件管理/Pods/Pods.h
@@ -193,6 +193,12 @@
 #import "TABAnimated.h"
 #endif
 
+#if __has_include(<WMZBanner/WMZBannerView.h>)
+#import <WMZBanner/WMZBannerView.h>
+#else
+#import "WMZBannerView.h"
+#endif
+
 #if __has_include(<BEMCheckBox/BEMCheckBox.h>)
 #import <BEMCheckBox/BEMCheckBox.h>
 #else

5d8da2573bd2d41234c96851432af72a6228648d
Jobs <lg295060456@gmail.com>
2021-02-03 12:02:05 +0700

    no message

--- My_BaseProj/🇨🇳ServiceLogic/Common/网络请求/App 的所有网络请求Api/NetworkingAPI.h
+++ My_BaseProj/🇨🇳ServiceLogic/Common/网络请求/App 的所有网络请求Api/NetworkingAPI.h
@@ -11,10 +11,15 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NetworkingAPI : NSObject
-
+// 不需要错误回调的
 +(void)requestApi:(NSString *_Nonnull)requestApi
        parameters:(id _Nullable)parameters
      successBlock:(MKDataBlock)successBlock;
+// 需要错误回调的
++(void)requestApi:(NSString *_Nonnull)requestApi
+       parameters:(id _Nullable)parameters
+     successBlock:(MKDataBlock)successBlock
+     failureBlock:(MKDataBlock)failureBlock;
 
 @end
 
